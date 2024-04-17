19a5a42eee96a7a9c5fa706bf14b1ea6f13e74a5
aboynukara <aboynukara@stud.hs-bremen.de>
2024-04-17 14:15:45 +0200

    Habe die Klasse Mitarbeiter erstellt/gemacht

--- src/valueobjects/Mitarbeiter.java
+++ src/valueobjects/Mitarbeiter.java
@@ -1,7 +1,20 @@
 package src.valueobjects;
 
 public class Mitarbeiter {
-    public static void main (String[]args){
-        System.out.println("Mitarbeiter");
+    private String name;
+    private int nummer;
+
+    public Mitarbeiter(String name, int nummer){
+        this.name = name;
+        this.nummer = nummer;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNummer() {
+        return nummer;
     }
 }
