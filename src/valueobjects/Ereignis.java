27d55f67ac6aa5153eabf79b3161fa79c02c133b
aylinx04 <165890967+aylinx04@users.noreply.github.com>
2024-05-14 14:48:18 +0200

    Klasse Ereignis

---
+++ src/valueobjects/Ereignis.java
@@ -0,0 +1,22 @@
+package src.valueobjects;
+
+import java.time.LocalDate;
+
+public class Ereignis {
+    private LocalDate datum;
+    private String status;
+
+    public Ereignis(){
+        this.datum = datum;
+        this.status = status;
+    }
+
+    public LocalDate getDatum() {
+        return datum;
+    }
+
+    @Override
+    public String toString() {
+        return "Ereignis:" + datum + "\n" + status;
+    }
+}
